666c026baf9e154142a435061b1786350de2b117
siningtong <siningtong@gmail.com>
2020-01-14 21:43:27 +0000

    display random cards

--- src/Deck.js
+++ src/Deck.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Card from "./Card";
 import axios from "axios";
 const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 class Deck extends Component {
@@ -14,26 +15,38 @@ class Deck extends Component {
   }
   getCard() {
     let id = this.state.deck.deck_id;
-    let cardUrl = `${API_BASE_URL}/${id}/draw/`;
-    axios.get(cardUrl).then(result => {
-      let card = result.data.cards[0];
-      this.setState(prev => ({
-        drawn: [
-          ...prev.drawn,
-          {
-            id: card.code,
-            image: card.image,
-            name: `${card.suit} ${card.value}`
-          }
-        ]
-      }));
-    });
+    try {
+      let cardUrl = `${API_BASE_URL}/${id}/draw/`;
+      axios.get(cardUrl).then(result => {
+        if (!result.data.success) {
+          throw new Error("No Card Remaining");
+        } else {
+          let card = result.data.cards[0];
+          this.setState(prev => ({
+            drawn: [
+              ...prev.drawn,
+              {
+                id: card.code,
+                image: card.image,
+                name: `${card.value} of ${card.suit}`
+              }
+            ]
+          }));
+        }
+      });
+    } catch (err) {
+      alert(err);
+    }
   }
   render() {
+    const cards = this.state.drawn.map(card => (
+      <Card key={card.id} name={card.name} image={card.image} />
+    ));
     return (
       <div>
         <h1>Card Dealer</h1>
         <button onClick={this.getCard}>Get Card </button>
+        {cards}
       </div>
     );
   }
