7b45e44e564485d92026f01ad06a1740f5120c79
siningtong <siningtong@gmail.com>
2020-01-14 21:07:09 +0000

    can fetch cards from API

--- src/Deck.js
+++ src/Deck.js
@@ -1,18 +1,41 @@
 import React, { Component } from "react";
 import axios from "axios";
+const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 class Deck extends Component {
   constructor(props) {
     super(props);
-    this.state = { deck: null };
+    this.state = { deck: null, drawn: [] };
+    this.getCard = this.getCard.bind(this);
   }
   componentDidMount() {
-    const url = "https://deckofcardsapi.com/api/deck/new/shuffle";
-    axios.get(url).then(response => {
-      console.log(response);
+    axios.get(`${API_BASE_URL}/new/shuffle/`).then(result => {
+      this.setState({ deck: result.data });
+    });
+  }
+  getCard() {
+    let id = this.state.deck.deck_id;
+    let cardUrl = `${API_BASE_URL}/${id}/draw/`;
+    axios.get(cardUrl).then(result => {
+      let card = result.data.cards[0];
+      this.setState(prev => ({
+        drawn: [
+          ...prev.drawn,
+          {
+            id: card.code,
+            image: card.image,
+            name: `${card.suit} ${card.value}`
+          }
+        ]
+      }));
     });
   }
   render() {
-    return <div>hello</div>;
+    return (
+      <div>
+        <h1>Card Dealer</h1>
+        <button onClick={this.getCard}>Get Card </button>
+      </div>
+    );
   }
 }
 export default Deck;
